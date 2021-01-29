587bd0c7da305efe72aa7136fa2d36949dd1bd15
Calvin Kuhnen <ckuhnen@hawk.iit.edu>
2021-01-28 20:54:19 -0600

    fixed recursion logic, loader working ok

--- src/Components/Radio.js
+++ src/Components/Radio.js
@@ -13,25 +13,26 @@ const Radio = () => {
     audio.play();
   }
 
+  // check for stream availability every 10 seconds unless it is already live
   const loader = async () => {
-    await new Promise(r => setTimeout(r, 5000));
-    if (!playable && status != "yay") {
+    if (status != "yay" && audio.networkState === 3) {
       audio.load();
-      await new Promise(r => setTimeout(r, 10000));
-      setStatus("loading...");
-      loader();
       console.log("called");
+      await new Promise(r => setTimeout(r, 10000));
+      loader();
     }
     else {
       setStatus("yay");
     }
   }
 
+  // first check of stream for fastest load possible
   audio.oncanplay = () => {
     playable = true;
     setStatus("yay");
   }
 
+  // call loader if stream not live (status not changed)
   if (status === "loading...") {
     loader();
   }
