97523eff5f4866accc38031d9f38a10e74961962
Calvin Kuhnen <ckuhnen@hawk.iit.edu>
2021-02-04 17:45:19 -0600

    added chat title

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Chat = () => {
   return (
     <div id="chat">
+      the sock drawer
       <iframe id="minnit" src="https://minnit.chat/sockDrawer?embed&&nickname="></iframe>
     </div>
   )
