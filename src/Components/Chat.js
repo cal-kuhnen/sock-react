6fa98fb4dc2c10ef48f9530befd9f7115bd2618e
Calvin Kuhnen <calvinkuhnen@gmail.com>
2022-07-14 03:08:59 -0400

    updated live bar and other css

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Chat = () => {
+
+  const [isExpanded, setExpanded] = useState(false);
+
+  const collapseChat = () => {
+    setExpanded(!isExpanded);
+  }
   return (
     <div id="chat">
-      the sock drawer
-      <iframe id="minnit" src="https://minnit.chat/sockDrawer?embed&&nickname="></iframe>
+      <div className='chat-button' onClick={collapseChat}>
+        the sock drawer
+      </div>
+      <iframe className={isExpanded ? 'minnit expanded' : 'minnit'} title='sock radio chat box' src="https://minnit.chat/sockDrawer?embed&&nickname="></iframe>
     </div>
   )
 }
