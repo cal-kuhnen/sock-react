812c3f1526151a0479fb406deeb3bb51aabc27ad
Calvin Kuhnen <calvinkuhnen@gmail.com>
2022-07-26 16:40:01 -0400

    more styling updates for new design

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,12 +4,11 @@ import '../css/home.css';
 const Home = () => {
   return(
     <div>
-    <div className="h-content">
-      <div className="footer">
-        <p>contact us: hello@sock.rocks</p>
+      <div className="h-content">
       </div>
-    </div>
-
+      <div className="footer">
+          <p>contact us: hello@sock.rocks</p>
+      </div>
     </div>
   )
 }
